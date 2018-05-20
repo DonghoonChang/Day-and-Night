cc6be2e52e35cb019c9d421a8120b5326db7d9c9
Donghoon(Jack) <varensio3@gmail.com>
2018-05-20 16:04:00 +1000

    Bullet Impact(Metallic) Done

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,21 +6,23 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] [Range(0, 200)] private int m_Health = 200;
-    [SerializeField] [Range(1f, 10f)] private float m_Speed_Walk = 5f;
-    [SerializeField] [Range(5f, 20f)] private float m_Speed_Run = 10f;
-    [SerializeField] [Range(5f, 20f)] private float m_Sensitivity_X = 5f;
-    [SerializeField] [Range(5f, 20f)] private float m_Sensitivity_Y = 5f;
+    [SerializeField] [Range(1f, 10f)] private float m_SpeedWalk = 5f;
+    [SerializeField] [Range(5f, 20f)] private float m_SpeedRun = 10f;
+    [SerializeField] [Range(5f, 20f)] private float m_SensitivityX = 5f;
+    [SerializeField] [Range(5f, 20f)] private float m_SensitivityY = 5f;
+    [SerializeField] [Range(20f, 5f)] private float m_PushForce = 30f;
     [SerializeField] private float m_gravityMultiplier = 2f;
     [SerializeField] bool m_isWalking = true;
 
     public GameObject cam;
-    public CharacterController character_ctrl;
+    public CharacterController characterCtrl;
+    public GameObject bulletMark;
     private float yaw = 0;
     private float pitch = 0;
 
     void Awake()
     {
-        character_ctrl = GetComponent<CharacterController>();
+        characterCtrl = GetComponent<CharacterController>();
     }
     // Use this for initialization
     void Start () {
@@ -35,34 +37,34 @@ public class Player : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        yaw += Input.GetAxis("Mouse Y") * m_Sensitivity_Y;
+        yaw += Input.GetAxis("Mouse Y") * m_SensitivityY;
         yaw = Mathf.Min(yaw, 90f);
         yaw = Mathf.Max(yaw, -90f);
-        pitch += Input.GetAxis("Mouse X") * m_Sensitivity_X;
+        pitch += Input.GetAxis("Mouse X") * m_SensitivityX;
         pitch %= 360f;
 
         transform.eulerAngles = new Vector3(0, pitch, 0);
         cam.transform.localEulerAngles = new Vector3(-yaw, 0, 0);
 
-        float speed_multi = m_isWalking ? m_Speed_Walk : m_Speed_Run;
+        float speed_multi = m_isWalking ? m_SpeedWalk : m_SpeedRun;
         Vector3 move_dir = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized;
 
         RaycastHit hit;
-        if (Physics.SphereCast(transform.position, character_ctrl.radius, Vector3.down,
-            out hit, character_ctrl.height/2f, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+        if (Physics.SphereCast(transform.position, characterCtrl.radius, Vector3.down,
+            out hit, characterCtrl.height/2f, Physics.AllLayers, QueryTriggerInteraction.Ignore))
         {
             Debug.DrawLine(transform.position, hit.point, Color.green);
-            Debug.DrawRay(transform.position - Vector3.down * character_ctrl.height/2f, Vector3.ProjectOnPlane(move_dir, hit.normal), Color.yellow);
+            Debug.DrawRay(transform.position - Vector3.down * characterCtrl.height/2f, Vector3.ProjectOnPlane(move_dir, hit.normal), Color.yellow);
         }
 
         move_dir = Vector3.ProjectOnPlane(move_dir, hit.normal).normalized * speed_multi * Time.deltaTime;
 
-        if (!character_ctrl.isGrounded)
+        if (!characterCtrl.isGrounded)
         {
             move_dir += Physics.gravity * m_gravityMultiplier;
         }
 
-        character_ctrl.Move(move_dir);
+        characterCtrl.Move(move_dir);
 
         if (Input.GetButtonDown("Fire1"))
         {
@@ -70,6 +72,15 @@ public class Player : MonoBehaviour {
         }
     }
 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {   
+        if (hit.rigidbody != null)
+        {
+            Rigidbody rb = hit.rigidbody;
+            rb.AddForce((hit.transform.position - transform.position).normalized * m_PushForce, ForceMode.Force);
+        }
+
+    }
     private void Fire()
     {
         RaycastHit hit;
@@ -77,6 +88,8 @@ public class Player : MonoBehaviour {
         {
             Debug.Log(hit.transform.name);
             Debug.DrawRay(cam.transform.position, cam.transform.TransformDirection(Vector3.forward) * hit.distance, Color.red, 0.5f);
+            GameObject mark = Instantiate(bulletMark, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(mark, 0.5f);
         }
     }
 }
