931900e7f4616de9f8be5baab7070ec889084e5b
Donghoon(Jack) <varensio3@gmail.com>
2018-05-20 12:13:24 +1000

    Basic Player Movement & Shooting Done

---
+++ Assets/Script/FpsCamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsCamera : MonoBehaviour {
+
+    [Range (1f, 5f)]
+    public float sensitivity_X =  2.5f;
+    
+    [Range(1f, 5f)]
+    public float sensitivity_Y = 2.5f;
+
+    private float _yaw;
+    private float _pitch;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = !(Cursor.lockState == CursorLockMode.Locked);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        _yaw += Input.GetAxis("Mouse Y") * sensitivity_Y;
+        _pitch += Input.GetAxis("Mouse X") * sensitivity_X;
+
+
+        transform.eulerAngles = new Vector3(-_yaw, _pitch, 0);
+
+        // Use Quaternion.Slerp() for smooth rotation
+    }
+
+    private void Fire()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+        }
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,29 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour {
 
-    [Range(0, 200)]
-    public int health = 200;
+    [SerializeField] [Range(0, 200)] private int m_Health = 200;
+    [SerializeField] [Range(1f, 10f)] private float m_Speed_Walk = 5f;
+    [SerializeField] [Range(5f, 20f)] private float m_Speed_Run = 10f;
+    [SerializeField] [Range(5f, 20f)] private float m_Sensitivity_X = 5f;
+    [SerializeField] [Range(5f, 20f)] private float m_Sensitivity_Y = 5f;
+    [SerializeField] private float m_gravityMultiplier = 2f;
+    [SerializeField] bool m_isWalking = true;
 
-    [Range(1f, 10f)]
-    public float speed_walk = 5f;
-
-    [Range(5f, 20f)]
-    public float speed_run = 10f;
-
-    [Range(35f, 150f)]
-    public float mass = 85f;
-
+    public GameObject cam;
+    public CharacterController character_ctrl;
+    private float yaw = 0;
+    private float pitch = 0;
 
     void Awake()
     {
-        
+        character_ctrl = GetComponent<CharacterController>();
     }
     // Use this for initialization
     void Start () {
-        Debug.Log("Here");
-	}
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = !(Cursor.lockState == CursorLockMode.Locked);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -33,22 +35,48 @@ public class Player : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        if (Input.GetButton("Forward"))
+        yaw += Input.GetAxis("Mouse Y") * m_Sensitivity_Y;
+        yaw = Mathf.Min(yaw, 90f);
+        yaw = Mathf.Max(yaw, -90f);
+        pitch += Input.GetAxis("Mouse X") * m_Sensitivity_X;
+        pitch %= 360f;
+
+        transform.eulerAngles = new Vector3(0, pitch, 0);
+        cam.transform.localEulerAngles = new Vector3(-yaw, 0, 0);
+
+        float speed_multi = m_isWalking ? m_Speed_Walk : m_Speed_Run;
+        Vector3 move_dir = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized;
+
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position, character_ctrl.radius, Vector3.down,
+            out hit, character_ctrl.height/2f, Physics.AllLayers, QueryTriggerInteraction.Ignore))
         {
-            transform.Translate(Vector3.forward * speed_walk * Time.deltaTime);
-        }
-        if (Input.GetButton("Backward"))
-        {
-            transform.Translate(Vector3.back * speed_walk * Time.deltaTime);
-        }
-        if (Input.GetButton("Left"))
-        {
-            transform.Translate(Vector3.left * speed_walk * Time.deltaTime);
+            Debug.DrawLine(transform.position, hit.point, Color.green);
+            Debug.DrawRay(transform.position - Vector3.down * character_ctrl.height/2f, Vector3.ProjectOnPlane(move_dir, hit.normal), Color.yellow);
         }
 
-        if (Input.GetButton("Right"))
+        move_dir = Vector3.ProjectOnPlane(move_dir, hit.normal).normalized * speed_multi * Time.deltaTime;
+
+        if (!character_ctrl.isGrounded)
         {
-            transform.Translate(Vector3.right * speed_walk * Time.deltaTime);
+            move_dir += Physics.gravity * m_gravityMultiplier;
+        }
+
+        character_ctrl.Move(move_dir);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, Physics.AllLayers, QueryTriggerInteraction.Collide))
+        {
+            Debug.Log(hit.transform.name);
+            Debug.DrawRay(cam.transform.position, cam.transform.TransformDirection(Vector3.forward) * hit.distance, Color.red, 0.5f);
         }
     }
 }
